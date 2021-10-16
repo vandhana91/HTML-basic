2424de90ce4f9d4db3924dcfb437591a10154987
vandhana91 <85727736+vandhana91@users.noreply.github.com>
2021-10-16 16:39:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var num1=20;
+var num2=30;
+var num3="20";
+console.log(num1==num3);
+console.log(num1===num3);
+console.log(num1!=num2);
+console.log(num1!==num3);
+console.log(num1<num2);
+console.log(num1>num2);
+console.log(num1<=num2);
+console.log(num1>num2);
